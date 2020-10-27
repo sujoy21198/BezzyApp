8ce36c31bbefa34a7e1e786c595f9b7a3bd68d10
WT136 <sujoy.saha@webstep.in>
2020-10-27 17:14:52 +0530

    initial commit to git full functional app

--- Easy_Application/Easy_Application/app/src/main/java/com/bezzy/Ui/View/Model/Friendsnoti_adapter.java
+++ Easy_Application/Easy_Application/app/src/main/java/com/bezzy/Ui/View/Model/Friendsnoti_adapter.java
@@ -6,26 +6,4 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class Friendsnoti_adapter extends RecyclerView.Adapter<Friendsnoti_adapter>{
-    @NonNull
-    @Override
-    public Friendsnoti_adapter onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull Friendsnoti_adapter holder, int position) {
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
-}
-   public class FriendsViewholder extends RecyclerView.ViewHolder{
-
-    public FriendsViewholder(@NonNull View itemView) {
-        super(itemView);
-    }
-}
+public class Friendsnoti_adapter {}
